62c4ea3a828956a7f491075862f369b5f2607f9c
YohanBT <yohanjayasanka0044@gmail.com>
2017-11-13 17:16:15 +0530

    plane travesability check update

--- include/planner/navigation_planner.h
+++ include/planner/navigation_planner.h
@@ -208,7 +208,7 @@ class NavigationPlanner{
         std::vector<geometry_msgs::PoseStamped> publishPath(struct Graph_Node *node);
         pcl::PointCloud<pcl::PointXYZ>::Ptr calculateConvexHull(vector<pcl::PointXYZ> point_vector,int point_type);
         pcl::PointCloud<pcl::PointXYZ>::Ptr getConvexHull(float x_cordinate, float y_cordinate, float z_cordinate, int point_type, float box_dimension);
-        bool planeTraversabilityCheck(double a,double b,double c,double e, double x0,double y0,double z0, double d, int type);
+        bool planeTraversabilityCheck(float a,float b,float c,float e, float x0,float y0,float z0, float d, int type);
 
     public:
         /*
